10758b58d646f8224a9fe40d1518e2562d052ef4
crystaltine <donaldcenaaa@outlook.com>
2026-03-03 21:20:41 -0800

    add test data to index.tsx for dev qol

--- index.tsx
+++ index.tsx
@@ -4,6 +4,8 @@ import './style.css'
 
 import { GameRenderer } from './GameRenderer'
 
+import TEST_DATA from "./matches_example/match.json";
+
 createRoot(document.getElementById('root')!).render(
-  <GameRenderer />
+  <GameRenderer initialData={TEST_DATA} />
 )
