815ce7bce650a47a061522cdd8ac29871e17751e
Henry Liao <hzl@andrew.cmu.edu>
2026-03-04 01:48:24 -0500

    fix relative imports

--- spritesheet.ts
+++ spritesheet.ts
@@ -1,24 +1,37 @@
-/** size in px of a board tile on the canvas at 100% zoom */
+// 1. Explicitly import the images (your bundler handles the magic here)
+import playerGreenSrc from "./sprites/green_player.png";
+import playerBlueSrc from "./sprites/blue_player.png";
+import neutralTileSrc from "./sprites/neutral_tile.png";
+import hillTileSrc from "./sprites/hill_tile.png";
+import wallTileSrc from "./sprites/wall_tile.png";
+import greenBeaconSrc from "./sprites/green_beacon.png";
+import blueBeaconSrc from "./sprites/blue_beacon.png";
+import powerCellSrc from "./sprites/power_cell.png";
+import floatingPieceBottomSrc from "./sprites/floating_piece_bottom.png";
+import wallTopSrc from "./sprites/wall_top.png";
+import wallBottomSrc from "./sprites/wall_bottom.png";
+import wallLeftSrc from "./sprites/wall_left.png";
+import wallRightSrc from "./sprites/wall_right.png";
+import wallTopLeftSrc from "./sprites/wall_top_left.png";
+import wallTopRightSrc from "./sprites/wall_top_right.png";
+import wallBottomLeftSrc from "./sprites/wall_bottom_left.png";
+import wallBottomRightSrc from "./sprites/wall_bottom_right.png";
+
 export const PX_PER_TILE = 32;
 
 export enum Sprite {
     PLAYER_GREEN,
     PLAYER_BLUE,
-
     TILE_LIGHT,
     TILE_DARK,
     HILL_LIGHT,
     HILL_DARK,
     WALL,
-
     BEACON_GREEN,
     BEACON_BLUE,
-
     POWERUP_HEALTH,
     POWERUP_STAMINA,
-
     FLOATING_PIECE_BOTTOM,
-
     WALL_TOP,
     WALL_BOTTOM,
     WALL_LEFT,
@@ -29,39 +42,31 @@ export enum Sprite {
     WALL_BOTTOM_RIGHT,
 }
 
-// Helper to resolve paths relative to THIS file for Vite bundle/submodule discovery
-const resolveSprite = (path: string) => new URL(path, import.meta.url).href;
-
-/**
- * File paths for individual sprite images.
- * Each PNG in the `sprites/` folder has a semantic name, e.g. `blue_player.png`.
- */
+// 2. Assign the imported variables directly
 export const SPRITE_FILES = {
-    [Sprite.PLAYER_GREEN]: resolveSprite("./sprites/green_player.png"),
-    [Sprite.PLAYER_BLUE]: resolveSprite("./sprites/blue_player.png"),
+    [Sprite.PLAYER_GREEN]: playerGreenSrc,
+    [Sprite.PLAYER_BLUE]: playerBlueSrc,
 
-    // neutral tile is used for both light and dark base tiles
-    [Sprite.TILE_LIGHT]: resolveSprite("./sprites/neutral_tile.png"),
-    [Sprite.TILE_DARK]: resolveSprite("./sprites/neutral_tile.png"),
+    [Sprite.TILE_LIGHT]: neutralTileSrc,
+    [Sprite.TILE_DARK]: neutralTileSrc,
 
-    [Sprite.HILL_LIGHT]: resolveSprite("./sprites/hill_tile.png"),
-    [Sprite.HILL_DARK]: resolveSprite("./sprites/hill_tile.png"),
-    [Sprite.WALL]: resolveSprite("./sprites/wall_tile.png"),
+    [Sprite.HILL_LIGHT]: hillTileSrc,
+    [Sprite.HILL_DARK]: hillTileSrc,
+    [Sprite.WALL]: wallTileSrc,
 
-    // dedicated beacon / powerup sprites
-    [Sprite.BEACON_GREEN]: resolveSprite("./sprites/green_beacon.png"),
-    [Sprite.BEACON_BLUE]: resolveSprite("./sprites/blue_beacon.png"),
-    [Sprite.POWERUP_HEALTH]: resolveSprite("./sprites/power_cell.png"),
-    [Sprite.POWERUP_STAMINA]: resolveSprite("./sprites/power_cell.png"),
+    [Sprite.BEACON_GREEN]: greenBeaconSrc,
+    [Sprite.BEACON_BLUE]: blueBeaconSrc,
+    [Sprite.POWERUP_HEALTH]: powerCellSrc,
+    [Sprite.POWERUP_STAMINA]: powerCellSrc,
 
-    [Sprite.FLOATING_PIECE_BOTTOM]: resolveSprite("./sprites/floating_piece_bottom.png"),
-    [Sprite.WALL_TOP]: resolveSprite("./sprites/wall_top.png"),
-    [Sprite.WALL_BOTTOM]: resolveSprite("./sprites/wall_bottom.png"),
-    [Sprite.WALL_LEFT]: resolveSprite("./sprites/wall_left.png"),
-    [Sprite.WALL_RIGHT]: resolveSprite("./sprites/wall_right.png"),
+    [Sprite.FLOATING_PIECE_BOTTOM]: floatingPieceBottomSrc,
+    [Sprite.WALL_TOP]: wallTopSrc,
+    [Sprite.WALL_BOTTOM]: wallBottomSrc,
+    [Sprite.WALL_LEFT]: wallLeftSrc,
+    [Sprite.WALL_RIGHT]: wallRightSrc,
 
-    [Sprite.WALL_TOP_LEFT]: resolveSprite("./sprites/wall_top_left.png"),
-    [Sprite.WALL_TOP_RIGHT]: resolveSprite("./sprites/wall_top_right.png"),
-    [Sprite.WALL_BOTTOM_LEFT]: resolveSprite("./sprites/wall_bottom_left.png"),
-    [Sprite.WALL_BOTTOM_RIGHT]: resolveSprite("./sprites/wall_bottom_right.png"),
+    [Sprite.WALL_TOP_LEFT]: wallTopLeftSrc,
+    [Sprite.WALL_TOP_RIGHT]: wallTopRightSrc,
+    [Sprite.WALL_BOTTOM_LEFT]: wallBottomLeftSrc,
+    [Sprite.WALL_BOTTOM_RIGHT]: wallBottomRightSrc,
 } as const;
