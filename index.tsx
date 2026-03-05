fba9b41564ddf50b0ec5b70fac99435d616a7e3d
crystaltine <donaldcenaaa@outlook.com>
2026-03-04 23:11:54 -0800

    make devtest work again

--- index.tsx
+++ index.tsx
@@ -3,27 +3,35 @@ import { createRoot } from 'react-dom/client'
 import './style.css'
 
 import { GameRenderer } from './GameRenderer'
+import { GameProvider, useGame } from './useGame';
+import { GamePGN, MapData } from './types';
 
-import TEST_DATA from "./matches_example/match.json";
+import _TEST_PGN from "./matches_example/match.json";
+const TEST_PGN = TEMP_convertRCArraysToObjects(_TEST_PGN) as GamePGN;
+
+import _TEST_MAP_DATA from "./matches_example/test_map_data.json";
+import { TEMP_convertRCArraysToObjects } from './utils';
+const TEST_MAP_DATA = _TEST_MAP_DATA as MapData;
 
 function DevTest() {
 
-  const [currentTurn, setCurrentTurn] = React.useState(0);
+  const { renderedGameFrame, incrementRenderedGameFrame } = useGame();
 
   return (
     <div className='app-root'>
       <div className='controls'>
-        <h3>currentTurn: {currentTurn}</h3>
-        <button onClick={() => setCurrentTurn((prev) => Math.max(0, prev - 1))}>(-)</button>
-        <button onClick={() => setCurrentTurn((prev) => Math.min(TEST_DATA.turn_count - 1, prev + 1))}>(+)</button>
+        <h3>currentTurn: {renderedGameFrame}</h3>
+        <button onClick={() => incrementRenderedGameFrame(-1)}>(-)</button>
+        <button onClick={() => incrementRenderedGameFrame(1)}>(+)</button>
       </div>
 
-      <GameRenderer 
-      initialData={TEST_DATA} 
-      currentTurn={currentTurn} 
-      setCurrentTurn={setCurrentTurn} />
+      <GameRenderer />
     </div>
   );
 }
 
-createRoot(document.getElementById('root')!).render(<DevTest />);
+createRoot(document.getElementById('root')!).render(
+  <GameProvider initPGN={TEST_PGN} initMapData={TEST_MAP_DATA}>
+    <DevTest />
+</GameProvider>
+);
--- utils.ts
+++ utils.ts
@@ -1,4 +1,4 @@
-import { MapLoc, Symmetry_t } from "./types";
+import { GamePGN, MapLoc, Symmetry_t } from "./types";
 
 export function make2DArray<T>(width: number, height: number, defaultValue: T): T[][] {
   return Array.from({ length: height }, () =>
@@ -24,4 +24,40 @@ export function applySymmetry(mapLoc: MapLoc, symmetry: Symmetry_t, mapWidth: nu
 
 export function mergeArrays<T>(arr1: T[], arr2: T[]): T[] {
   return [...arr1, ...arr2];
+}
+
+/**
+ * THIS IS TEMPORARY - we should refactor the engine game output asap.
+ * 
+ * right now the game pgns outputted by the python engine define coords as arrays
+ * of [r, c], but in typescript we have explicit {r, c} keys to prevent confusion.
+ * 
+ * This just takes in data thats ALMOST the correct type (GamePGN), but just with
+ * the following keys as [r, c] arrays that need to be converted:
+ * - p1_loc[:] and p2_loc[:]
+ * - actions[:][:].location
+ * - actions[:][:].beacon_target
+ */
+export function TEMP_convertRCArraysToObjects(dataFromPython: any): GamePGN {
+  const ret = { ...dataFromPython };
+  
+  // convert all elements of p1_loc and p2_loc from [r, c] to {r, c}
+  ret.p1_loc = dataFromPython.p1_loc.map((loc: number[]) => ({ r: loc[0], c: loc[1] }));
+  ret.p2_loc = dataFromPython.p2_loc.map((loc: number[]) => ({ r: loc[0], c: loc[1] }));
+
+  // for all actions[i] that are arrays:
+  for (const turnActions of ret.actions) {
+    // for all action objects in each of these arrays
+    for (const actionObj of turnActions) {
+      // change .location or .beacon_target if it exists
+      if (actionObj.location) {
+        actionObj.location = { r: actionObj.location[0], c: actionObj.location[1] };
+      }
+      if (actionObj.beacon_target) {
+        actionObj.beacon_target = { r: actionObj.beacon_target[0], c: actionObj.beacon_target[1] };
+      }
+    }
+  }
+
+  return ret;
 }
