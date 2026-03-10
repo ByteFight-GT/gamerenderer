9c73965554c5ba8115f65c9a2d9b9b949ec29c92
crystaltine <donaldcenaaa@outlook.com>
2026-03-09 21:17:46 -0700

    add adaptive auto vs. pixelated rendering for canvas based on zoom

--- GameRenderer.tsx
+++ GameRenderer.tsx
@@ -18,6 +18,9 @@ type GameRendererProps = {
   }
 }
 
+/** scale at which to switch to pixelated rendering so pixel art looks better up close */
+const PIXELATED_RENDER_THRESHOLD = 2;
+
 const GamerendererDefaultHoverElement = () => (
   <div 
   style={{width: PX_PER_TILE, height: PX_PER_TILE}} 
@@ -32,6 +35,7 @@ export const GameRenderer = (props: GameRendererProps) => {
   const backgroundCanvasRef = React.useRef<HTMLCanvasElement>(null);
 
   const [hoverElementPos, setHoverElementPosition] = React.useState<{ x: number; y: number } | null>(null);
+  const [currZoomScale, setCurrZoomScale] = React.useState(1);
 
   React.useEffect(() => {
     _registerCanvases(spriteCanvasRef.current!, backgroundCanvasRef.current!);
@@ -68,7 +72,12 @@ export const GameRenderer = (props: GameRendererProps) => {
 
 
   return (
-    <TransformWrapper centerOnInit limitToBounds={false} minScale={0.1} {...props.transformWrapperProps}>
+    <TransformWrapper 
+    centerOnInit 
+    minScale={0.1} 
+    limitToBounds={false} 
+    onZoom={ref => setCurrZoomScale(ref.state.scale)} 
+    {...props.transformWrapperProps}>
       <TransformComponent {...props.transformComponentProps}>
         <div
         onMouseMove={handleMouseMove}
@@ -91,8 +100,19 @@ export const GameRenderer = (props: GameRendererProps) => {
             </div>
           )}
 
-          <canvas ref={backgroundCanvasRef} style={{imageRendering: "pixelated"}} className="col-start-1 row-start-1"/>
-          <canvas ref={spriteCanvasRef} style={{imageRendering: "pixelated"}} className="col-start-1 row-start-1" />
+          <canvas 
+          ref={backgroundCanvasRef} 
+          style={{
+            imageRendering: currZoomScale >= PIXELATED_RENDER_THRESHOLD? "pixelated" : "auto"
+          }} 
+          className="col-start-1 row-start-1" />
+
+          <canvas 
+          ref={spriteCanvasRef} 
+          style={{
+            imageRendering: currZoomScale >= PIXELATED_RENDER_THRESHOLD? "pixelated" : "auto"
+          }}
+          className="col-start-1 row-start-1" />
         </div>
       </TransformComponent>
     </TransformWrapper>
