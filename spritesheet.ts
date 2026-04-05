3ec0777d663b523c5e52707c444c344cac8404be
crystaltine <donaldcenaaa@outlook.com>
2026-04-05 10:44:47 -0700

    make sprites resolve to ./assets instead of public/ folder of outer repos

--- spritesheet.ts
+++ spritesheet.ts
@@ -41,49 +41,56 @@ export enum Sprite {
 	WALL_BOTTOM_LEFT,
 	WALL_BOTTOM_RIGHT,
 }
-/**
- * File paths for individual sprite images.
- * Each PNG in the `sprites/` folder has a semantic name, e.g. `blue_player.png`.
- */
-export const SPRITE_FILES = {
-	[Sprite.PLAYER_GREEN]: "/sprites/green_player.png",
-	[Sprite.PLAYER_BLUE]: "/sprites/blue_player.png",
+
+
+const SPRITE_PATHS = {
+	[Sprite.PLAYER_GREEN]: "green_player.png",
+	[Sprite.PLAYER_BLUE]: "blue_player.png",
 
 	// neutral tile is used for both light and dark base tiles
-	[Sprite.TILE_LIGHT]: "/sprites/neutral_tile.png",
-	[Sprite.TILE_DARK]: "/sprites/neutral_tile.png",
+	[Sprite.TILE_LIGHT]: "neutral_tile.png",
+	[Sprite.TILE_DARK]: "neutral_tile.png",
 
-	[Sprite.HILL_LIGHT]: "/sprites/hill_tile.png",
-	[Sprite.HILL_DARK]: "/sprites/hill_tile.png",
-	[Sprite.HILL_BORDER]: "/sprites/hill_border.png",
-	[Sprite.WALL]: "/sprites/wall_tile.png",
+	[Sprite.HILL_LIGHT]: "hill_tile.png",
+	[Sprite.HILL_DARK]: "hill_tile.png",
+	[Sprite.HILL_BORDER]: "hill_border.png",
+	[Sprite.WALL]: "wall_tile.png",
 
-	[Sprite.BLUE_TILE_1]: "/sprites/blue_tile_1.png",
-	[Sprite.BLUE_TILE_2]: "/sprites/blue_tile_2.png",
-	[Sprite.BLUE_TILE_3]: "/sprites/blue_tile_3.png",
-	[Sprite.BLUE_TILE_4]: "/sprites/blue_tile_4.png",
-	[Sprite.BLUE_SPAWN]: "/mapbuilder/BLUE_SPAWN.png",
+	[Sprite.BLUE_TILE_1]: "blue_tile_1.png",
+	[Sprite.BLUE_TILE_2]: "blue_tile_2.png",
+	[Sprite.BLUE_TILE_3]: "blue_tile_3.png",
+	[Sprite.BLUE_TILE_4]: "blue_tile_4.png",
+	[Sprite.BLUE_SPAWN]: "mapbuilder/BLUE_SPAWN.png",
 
-	[Sprite.GREEN_TILE_1]: "/sprites/green_tile_1.png",
-	[Sprite.GREEN_TILE_2]: "/sprites/green_tile_2.png",
-	[Sprite.GREEN_TILE_3]: "/sprites/green_tile_3.png",
-	[Sprite.GREEN_TILE_4]: "/sprites/green_tile_4.png",
-	[Sprite.GREEN_SPAWN]: "/mapbuilder/GREEN_SPAWN.png",
+	[Sprite.GREEN_TILE_1]: "green_tile_1.png",
+	[Sprite.GREEN_TILE_2]: "green_tile_2.png",
+	[Sprite.GREEN_TILE_3]: "green_tile_3.png",
+	[Sprite.GREEN_TILE_4]: "green_tile_4.png",
+	[Sprite.GREEN_SPAWN]: "mapbuilder/GREEN_SPAWN.png",
 
 	// dedicated beacon / powerup sprites
-	[Sprite.BEACON_GREEN]: "/sprites/green_beacon.png",
-	[Sprite.BEACON_BLUE]: "/sprites/blue_beacon.png",
-	[Sprite.POWERUP_HEALTH]: "/sprites/power_cell.png",
-	[Sprite.POWERUP_STAMINA]: "/sprites/power_cell.png",
+	[Sprite.BEACON_GREEN]: "green_beacon.png",
+	[Sprite.BEACON_BLUE]: "blue_beacon.png",
+	[Sprite.POWERUP_HEALTH]: "power_cell.png",
+	[Sprite.POWERUP_STAMINA]: "power_cell.png",
 
-	[Sprite.FLOATING_PIECE_BOTTOM]: "/sprites/floating_piece_bottom.png",
-	[Sprite.WALL_TOP]: "/sprites/wall_top.png",
-	[Sprite.WALL_BOTTOM]: "/sprites/wall_bottom.png",
-	[Sprite.WALL_LEFT]: "/sprites/wall_left.png",
-	[Sprite.WALL_RIGHT]: "/sprites/wall_right.png",
+	[Sprite.FLOATING_PIECE_BOTTOM]: "floating_piece_bottom.png",
+	[Sprite.WALL_TOP]: "wall_top.png",
+	[Sprite.WALL_BOTTOM]: "wall_bottom.png",
+	[Sprite.WALL_LEFT]: "wall_left.png",
+	[Sprite.WALL_RIGHT]: "wall_right.png",
 
-	[Sprite.WALL_TOP_LEFT]: "/sprites/wall_top_left.png",
-	[Sprite.WALL_TOP_RIGHT]: "/sprites/wall_top_right.png",
-	[Sprite.WALL_BOTTOM_LEFT]: "/sprites/wall_bottom_left.png",
-	[Sprite.WALL_BOTTOM_RIGHT]: "/sprites/wall_bottom_right.png",
-} as const;
+	[Sprite.WALL_TOP_LEFT]: "wall_top_left.png",
+	[Sprite.WALL_TOP_RIGHT]: "wall_top_right.png",
+	[Sprite.WALL_BOTTOM_LEFT]: "wall_bottom_left.png",
+	[Sprite.WALL_BOTTOM_RIGHT]: "wall_bottom_right.png",
+} as const satisfies Record<Sprite, string>;
+
+// so that we dont need to shove these assets in the public/ folder
+// of whatever app is using this submodule
+export const SPRITE_FILES = Object.fromEntries(
+	Object.entries(SPRITE_PATHS).map(([sprite, path]) => [
+		sprite,
+		new URL(`./assets/${path}`, import.meta.url).href
+	]),
+) as Record<Sprite, string>;
