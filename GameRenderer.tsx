1c9059420787bf363f1a255cb50022ec202c0174
crystaltine <donaldcenaaa@outlook.com>
2026-04-06 22:23:20 -0700

    allow customization of both canvas and wrapper props for GameRenderer

--- GameRenderer.tsx
+++ GameRenderer.tsx
@@ -2,29 +2,41 @@ import React from "react";
 
 import { useVisualizer } from "./useVisualizer";
 
+const DEFAULT_WRAPPER_STYLE = {
+  position: "relative", 
+  display: "grid"
+} as const;
 const CANVAS_STYLE = {
   gridColumnStart: 1, 
   gridRowStart: 1, 
-  height: "100%", 
-  width: "100%",
   imageRendering: "pixelated",
 } as const;
 
-export const GameRenderer = (props: React.HTMLAttributes<HTMLDivElement>) => {
-  const {canvasManagerRef, _registerCanvases} = useVisualizer();
+type GameRendererProps = {
+  wrapperProps?: React.HTMLAttributes<HTMLDivElement>;
+  canvasProps?: React.CanvasHTMLAttributes<HTMLCanvasElement>;
+};
+
+export const GameRenderer = ({ wrapperProps, canvasProps }: GameRendererProps) => {
+  const { _registerCanvases } = useVisualizer();
 
   const spriteCanvasRef = React.useRef<HTMLCanvasElement>(null);
   const backgroundCanvasRef = React.useRef<HTMLCanvasElement>(null);
 
   React.useEffect(() => {
     _registerCanvases(spriteCanvasRef.current!, backgroundCanvasRef.current!);
-  }, [_registerCanvases, canvasManagerRef]);
+  }, [_registerCanvases]);
 
-  const { style, ...rest } = props;
+  const { style: wrapperStyle, ...wrapperRest } = wrapperProps ?? {};
+  const { style: canvasStyle, ...canvasRest } = canvasProps ?? {};
+
+  const mergedWrapperStyle = { ...DEFAULT_WRAPPER_STYLE, ...wrapperStyle };
+  const mergedCanvasStyle = { ...CANVAS_STYLE, ...canvasStyle };
+
   return (
-    <div style={{ position: "relative", display: "grid", ...style }} {...rest}>
-      <canvas ref={backgroundCanvasRef} style={CANVAS_STYLE} />
-      <canvas ref={spriteCanvasRef} style={CANVAS_STYLE} />
+    <div style={{ ...mergedWrapperStyle }} {...wrapperRest}>
+      <canvas {...canvasRest} ref={backgroundCanvasRef} style={mergedCanvasStyle} />
+      <canvas {...canvasRest} ref={spriteCanvasRef} style={mergedCanvasStyle} />
     </div>
   );
 };
